1563e44881a7bc011fe2165482503680c2621c10
smoly87 <smoly87@gmail.com>
2014-03-03 23:03:27 +0700

    Add  domain structure.

---
+++ src/domainSample/entity/AirplaneTransportService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domainSample.entity;
+
+/**
+ *
+ * @author Andrey
+ */
+public class AirplaneTransportService extends TransportService{
+    
+}
